cc1c2225ca3be16d98bb01b689661080322b6399
Cyonge <55596927+Cyonge@users.noreply.github.com>
2019-11-15 12:40:08 -0600

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,9 +1,15 @@
 //VARIABLES
 //================================================
 var apiKey = "&appid=8828f41ba465ab0fe84b7b789254b6e7";
-var urlBase = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=london" + apiKey;
+var urlBase = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=" + apiKey;
 var searchedCity = "";
 var cityHistory = [];
+var d = new Date();
+var strDate = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate();
+var currentTime = new Date().toLocaleTimeString();
+
+
+
 
 //FUNCTIONS
 //================================================
@@ -11,32 +17,46 @@ var cityHistory = [];
 // This is for the API call
 
 
-function citySearch(urlBase){
+function citySearch(weatherData, queryUrl){
     $.ajax({
         url: urlBase,
         method: "GET"
     }).then(function (weatherData) {
         
-        console.log(weatherData);
-       
-});
-console.log('hello')
+    
+    console.log(weatherData);
+    console.log(weatherData.main.humidity);
+    console.log(weatherData.main.temp);
+    console.log(weatherData);
+    console.log(weatherData.weather[0].description)
+
+    console.log(currentTime);
+    console.log(strDate);
+        });
+
 
 };
 
 // //Button for searching a city
-$("#button").on('click', function(event) {
+$("#searchBtn").on('click', function(event) {
     event.preventDefault();
-    searchedCity = $("#form-control").text().trim();
+    searchedCity = $("#form-control").val().trim();
+    var newUrl = urlBase + "&q=" + searchedCity; 
+    console.log("hello");
+    
 
     //variable for the new URL created after the search
-    var newUrl = urlBase + "&q=" + searchedCity;   
+      
+
+    
 
     
 });
+
 citySearch();
 
-console.log(urlBase);
+
+
 
 
 //MAIN PROCESSES
@@ -47,5 +67,7 @@ console.log(urlBase);
     // b) This makes an AJAX request to the weather API for whatever city that was
     // c) Based on whatever city is searched it queries everything on the page
     // d) Will need to clear input out of search bar after and clear page when a new search is made
+
+
 
 
