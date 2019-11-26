5121f2c80b227b84ec7ee4965b47e6786d8dccd9
Christian Yonge <christianyonge93@gmail.com>
2019-11-26 16:08:31 -0600

    pushing up completed material for TA

--- logic.js
+++ logic.js
@@ -2,11 +2,18 @@
 //================================================
 var apiKey = "&appid=8828f41ba465ab0fe84b7b789254b6e7";
 var urlBase = "https://api.openweathermap.org/data/2.5/weather?" + apiKey;
-var searchedCity = "";
-var cityHistory = [];
+var fiveDayUrlBase = "https://api.openweathermap.org/data/2.5/forecast?" + apiKey;
+var cityInput = "";
 var d = new Date();
-var strDate = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
+var strDate = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
 var currentTime = new Date().toLocaleTimeString();
+
+
+
+// this is to capitalize the first letter in a string
+function uppercaseFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+    }
 
 
 $(document).ready(function () {
@@ -14,7 +21,7 @@ $(document).ready(function () {
     //FUNCTIONS
     //================================================
 
-    // This is for the API call
+// This is for the API call
 
 
     function getWeather(UrlToSearch) {
@@ -24,38 +31,64 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (weatherData) {
 
-            // console.log to check if the AJAX call is accessing the API
+// console.log to check if the AJAX call is accessing the API
             console.log(weatherData);
-            console.log(weatherData.main.humidity);
-            console.log(weatherData.main.temp);
-            console.log(weatherData);
-            console.log(weatherData.weather[0].description)
 
-            console.log(currentTime);
-            console.log(strDate);
-
+// this section adds the requested weather data to the page
             $('#humidity').text(weatherData.main.humidity + "%");
+            $('#date').text(strDate);
+            $('#temp').text(weatherData.main.temp + ' F');
+            $('#rain').text(uppercaseFirstLetter(weatherData.weather[0].description));
+            // how do I get the time to update every time I search?
+            $('#time').text(currentTime);
+
+
+
+
         });
 
-
-
-
-
     };
-// // //Button for searching a city
+
+// function that will call the 5 day forecast
+function fiveDayForecast(UrlToSearch2){
+    
+    // AJAX call for the 5 day forecast
+    $.ajax({
+        url: UrlToSearch2,
+        method: "GET"
+    }).then(function(forecast){
+        console.log(forecast)
+        
+
+    }
+
+)};
+
+
+//Button for searching a city
 $("#searchBtn").on('click', function () {
     event.preventDefault();
-    var searchedCity = $("#form-control").val().trim();
-    var newUrl = urlBase + "&q=" + searchedCity;
-    console.log("hello");
 
+    var cityInput = $("#form-control").val().trim();
+    // building our URL here
+    var newUrl = urlBase + "&q=" + cityInput + "&units=imperial";
+    
+    
+// this section will show the city History on the left side of the screen
+    // setting a variable equal to what we want rendered on the page
+    var cityList = $(`
+    <li class="list-group-item">${cityInput}</li>
+    `);
+    // this prepends to city that you search based on the variable cityList
+    $(".list-group").prepend(cityList);
+    console.log(cityList);
 
-    //variable for the new URL created after the search
-    console.log(newUrl)
+// this section of the button will be for the 5 day forecast URL and adding data to the page
+    var newFiveDayUrl = fiveDayUrlBase + "&q=" + cityInput + "&units=imperial"
 
-    getWeather(newUrl);
-
-
+getWeather(newUrl);
+fiveDayForecast(newFiveDayUrl);
+    
 
 
 });
